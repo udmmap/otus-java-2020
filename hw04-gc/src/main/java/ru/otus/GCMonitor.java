05d89e804308016963ed3d622f19c0ec3cf43748
Дмитрий Корепанов <dpurum@yandex.ru>
2020-11-04 16:26:04 +0300

    Доработки

--- hw04-gc/src/main/java/ru/otus/GCMonitor.java
+++ hw04-gc/src/main/java/ru/otus/GCMonitor.java
@@ -16,39 +16,65 @@ public class GCMonitor {
 
         switchOnMonitoring();
 
-        long beginTime = System.currentTimeMillis();
-
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
         ObjectName name = new ObjectName("ru.otus:type=Consumer");
 
         Consumer mbean = new Consumer();
         mbs.registerMBean(mbean, name);
-        mbean.run();
 
-        System.out.println("time:" + (System.currentTimeMillis() - beginTime) / 1000);
+        beginTime = System.currentTimeMillis();
+        mbean.run();
 
     }
 
+    private static long totalDuration;
+    private static long beginTime;
+
+    private static NotificationListener listener = (notification, handback) -> {
+        if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+            GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+            String gcName = info.getGcName();
+            String gcAction = info.getGcAction();
+            String gcCause = info.getGcCause();
+
+            long startTime = info.getGcInfo().getStartTime();
+            long duration = info.getGcInfo().getDuration();
+            totalDuration += duration;
+            System.out.println(
+                    "time:" + (System.currentTimeMillis() - beginTime) / 1000
+                    + " totalGC:" + totalDuration
+                    + " Name:" + gcName
+                    + " action:" + gcAction
+                    + " gcCause:" + gcCause
+                    + "(" + duration + " ms)");
+            info.getGcInfo().getMemoryUsageAfterGc().forEach((key,memUsed)->{
+                System.out.print(String.format(" %s:%d",key,memUsed.getUsed()));
+            });
+            System.out.println("");
+        }
+    };
+
     private static void switchOnMonitoring() {
-        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
-        for (GarbageCollectorMXBean gcbean : gcbeans) {
-            System.out.println("GC name:" + gcbean.getName());
-            NotificationEmitter emitter = (NotificationEmitter) gcbean;
-            NotificationListener listener = (notification, handback) -> {
-                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
-                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
-                    String gcName = info.getGcName();
-                    String gcAction = info.getGcAction();
-                    String gcCause = info.getGcCause();
-
-
-                    long startTime = info.getGcInfo().getStartTime();
-                    long duration = info.getGcInfo().getDuration();
-
-                    System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
-                }
-            };
+        List<GarbageCollectorMXBean> gcBeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcBean : gcBeans) {
+            System.out.println("GC name:" + gcBean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcBean;
+            totalDuration = 0;
             emitter.addNotificationListener(listener, null, null);
+        }
+    }
+
+    private static void switchOffMonitoring() {
+        List<GarbageCollectorMXBean> gcBeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcBean : gcBeans) {
+            System.out.println("GC name:" + gcBean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcBean;
+
+            try {
+                emitter.removeNotificationListener(listener, null, null);
+            } catch (ListenerNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 
