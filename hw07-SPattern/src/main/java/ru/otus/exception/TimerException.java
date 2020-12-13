f92da3e0f785e6c9facde79c5c42bd6040147d89
Дмитрий Корепанов <dpurum@yandex.ru>
2020-12-13 14:40:27 +0300

    HW07, structural patterns TimerException

--- hw07-SPattern/src/main/java/ru/otus/exception/TimerException.java
+++ hw07-SPattern/src/main/java/ru/otus/exception/TimerException.java
@@ -3,9 +3,4 @@ package ru.otus.exception;
 import java.time.LocalDateTime;
 
 public class TimerException extends Exception {
-    private final LocalDateTime creationTime = LocalDateTime.now();
-
-    public LocalDateTime getCreationTime() {
-        return creationTime;
-    }
 }
