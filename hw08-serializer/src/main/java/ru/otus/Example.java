ac72188b1b408e9214cd5980a4aba38d4f67b849
Дмитрий Корепанов <dpurum@yandex.ru>
2020-12-15 18:05:41 +0300

    hw08 Serializer

---
+++ hw08-serializer/src/main/java/ru/otus/Example.java
@@ -0,0 +1,15 @@
+package ru.otus;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Example {
+    int mi = 789;
+    Float mF = 7.89f;
+    String mS = "String";
+    Double[] mAD = new Double[]{0.7d, 0.8d, 0.9d};
+    double[] mAd = new double[]{0.7d, 0.8d, 0.9d};
+    Boolean mB = true;
+    Collection mCS = Arrays.asList("Str","ing");
+    Integer mIN = null;
+}
